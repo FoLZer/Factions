5f316c97a5dacff5d6535cbb6a5ca994c2441d24
FoLZer <nikolaenko.gleb.044@gmail.com>
2020-08-29 04:04:00 +0300

    Remove sentinel as it's not needed

--- src/main/java/com/massivecraft/factions/integration/IntegrationManager.java
+++ src/main/java/com/massivecraft/factions/integration/IntegrationManager.java
@@ -41,7 +41,6 @@ public class IntegrationManager implements Listener {
         LWC("LWC", com.massivecraft.factions.integration.LWC::setup),
         PLACEHOLDERAPI("PlaceholderAPI", (p) -> FactionsPlugin.getInstance().setupPlaceholderAPI()),
         PLACEHOLDERAPI_OTHER("MVdWPlaceholderAPI", (p) -> FactionsPlugin.getInstance().setupOtherPlaceholderAPI()),
-        SENTINEL("Sentinel", Sentinel::init),
         WORLDGUARD("WorldGuard", (plugin) -> {
             FactionsPlugin f = FactionsPlugin.getInstance();
             if (!f.conf().worldGuard().isChecking() && !f.conf().worldGuard().isBuildPriority()) {
